c8858240da549112b2ab83956f57d9fe0c3a7462
Jonas Lucas <jonaslucasduarte@gmail.com>
2023-10-13 22:22:55 -0300

    Add tuples annotations

---
+++ Basic Typing/tuples.py
@@ -0,0 +1,8 @@
+"""About annotations of Tuples."""
+coordinates: tuple[int, int] = (20, 30)
+
+mixed: tuple[str, int, bool] = ('hello', 10, True)
+
+large: tuple[int, ...] = (10, 20, 30, 40, 50)
+
+var: tuple[int, str] = (10, 'hello')
